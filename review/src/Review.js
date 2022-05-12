9132a95134db3dec6bfb4d3dc49455d67ef2a785
spkspk122 <spkspk123456789@gmail.com>
2022-05-12 18:50:06 +0530

    v1.0

--- review/src/Review.js
+++ review/src/Review.js
@@ -94,11 +94,11 @@ function Review() {
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
+    <span class="visually-hidden"></span>
   </button>
   <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
+    <span class="visually-hidden"></span>
   </button>
 </div>
 
